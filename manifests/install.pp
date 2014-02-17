35a290ccbecc646a13cc59e3df0f905575b5eba7
Derrick Ampy <dsampy@gmail.com>
2014-02-17 17:33:26 -0500

    Updated install Puppet classes to download artifacts

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,15 @@
 class tomcat::install {
+	wget::fetch { "Download Tomcat":
+	  source      => 'http://mirror.sdunix.com/apache/tomcat/tomcat-7/v7.0.50/bin/apache-tomcat-7.0.50.tar.gz',
+	  destination => '/tmp/apache-tomcat-7.0.50.tar.gz',
+	  timeout     => 0,
+	  verbose     => false,
+	}
 
+	wget::fetch { "Download Tomcat Native Connector":
+	  source      => 'http://mirror.nexcess.net/apache//tomcat/tomcat-connectors/native/1.1.29/source/tomcat-native-1.1.29-src.tar.gz',
+	  destination => '/tmp/tomcat-native-1.1.29-src.tar.gz',
+	  timeout     => 0,
+	  verbose     => false,
+	}
 }
