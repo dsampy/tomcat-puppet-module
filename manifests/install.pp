94863f0f5a0e01193d239a2a4fdec33835b899b9
Derrick Ampy <dsampy@gmail.com>
2014-02-17 20:26:27 -0500

    Updated Puppet install classes to use nexus repository

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,13 +1,13 @@
 class tomcat::install {
 	wget::fetch { "Download Tomcat":
-	  source      => 'http://mirror.sdunix.com/apache/tomcat/tomcat-7/v7.0.50/bin/apache-tomcat-7.0.50.tar.gz',
+	  source      => 'http://10.0.2.2:8081/nexus/service/local/repositories/public/content/org/apache/tomcat/tomcat/7.0.50/tomcat-7.0.50.tar.gz',
 	  destination => '/tmp/apache-tomcat-7.0.50.tar.gz',
 	  timeout     => 0,
 	  verbose     => false,
 	}
 
 	wget::fetch { "Download Tomcat Native Connector":
-	  source      => 'http://mirror.nexcess.net/apache//tomcat/tomcat-connectors/native/1.1.29/source/tomcat-native-1.1.29-src.tar.gz',
+	  source      => 'http://10.0.2.2:8081/nexus/service/local/repositories/public/content/org/apache/tomcat/tomcat-native/1.1.29/tomcat-native-1.1.29-src.tar.gz',
 	  destination => '/tmp/tomcat-native-1.1.29-src.tar.gz',
 	  timeout     => 0,
 	  verbose     => false,
